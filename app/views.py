08891e3401c02a44510fe198cf06e1e9647476e5
Anurag30112003 <anuragsharma011011@gmail.com>
2021-10-03 17:41:20 +0530

    major commits

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint , render_template,request
+from flask import Blueprint , render_template,request,flash, redirect, url_for
 from flask_login import  login_required , current_user
+from .models import Post, User, Comment
+from . import db
 
 views = Blueprint('views',__name__)
 
@@ -17,7 +19,101 @@ def about():
 def dashboard():
     return render_template('dashboard.html',user = current_user)
 
+@views.route('/endanger')
+@login_required
+def endanger():
+    return render_template('endanger.html',user = current_user)
+
+@views.route('/clean')
+@login_required
+def clean():
+    return render_template('clean.html',user = current_user)
+
 @views.route('/api')
 @login_required
 def ind():
     return render_template('iden.html')
+
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('create_post.html', user=current_user)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id != post.id:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=name).first()
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = user.posts
+    return render_template("posts.html", user=current_user, posts=posts, username=name)
+
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
